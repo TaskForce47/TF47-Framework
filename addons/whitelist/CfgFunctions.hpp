0a28a0c1cae3a63f561f2a3c4c152f68877c9351
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-07-04 17:47:44 +0200

    hello tabs my old friend, i've come to remove you again

--- addons/whitelist/CfgFunctions.hpp
+++ addons/whitelist/CfgFunctions.hpp
@@ -6,17 +6,17 @@ class CfgFunctions
         {
             file = "\tf47\tf47_framework\addons\whitelist\functions";
             class checkRoles {};
-			class checkWhitelistSlot {};
-			class handleRoleCheck {};
-			class handleSlotCheck {};
-			class initPlayer { postInit = 1; };
-			class initServer { postInit = 1; };
-			class initSlotRoles {};
-			class isValidVehicle {};
-			class kickFromVehicle {};
-			class kickToLobby {};
-			class registerRoles {};
-			class registerSlotWhitelist {};
+            class checkWhitelistSlot {};
+            class handleRoleCheck {};
+            class handleSlotCheck {};
+            class initPlayer { postInit = 1; };
+            class initServer { postInit = 1; };
+            class initSlotRoles {};
+            class isValidVehicle {};
+            class kickFromVehicle {};
+            class kickToLobby {};
+            class registerRoles {};
+            class registerSlotWhitelist {};
         };
     };
 };
